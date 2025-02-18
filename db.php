04f73a3146a7a3fc982eddf52ae47cc4109d01bd
Julio Pazzaglia <julitopazzaglia@gmail.com>
2025-02-18 17:51:25 -0300

    correcciones a DB y creacion y seed de Questions
    corregi algunas cosas de la estructura que no estaba ok y cree el seed de las preguntas. Falta refactorizar

--- db.php
+++ db.php
@@ -24,7 +24,7 @@ $conn->select_db($dbname);
 
 // Crear tabla Players
 $sql = "CREATE TABLE IF NOT EXISTS Players (
-    number INT AUTO_INCREMENT PRIMARY KEY,
+    number INT PRIMARY KEY,
     name CHAR(255) NOT NULL,
     votes INT NOT NULL,
     hasVoted BOOLEAN NOT NULL
@@ -36,10 +36,23 @@ if ($conn->query($sql) === TRUE) {
     echo "Error creating table: " . $conn->error;
 }
 
+
+// Crear tabla Questions
+$sql = "CREATE TABLE IF NOT EXISTS Questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    questions TEXT NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Game created successfully\n";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
 // Crear tabla Game con relación a Players
 $sql = "CREATE TABLE IF NOT EXISTS Game (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    questions TEXT NOT NULL, -- Usar TEXT para almacenar arrays serializados pero puedes usar serialize() y unserialize() 
+    questionsId TEXT NOT NULL, -- Usar TEXT para almacenar arrays serializados pero puedes usar serialize() y unserialize() 
     player_id INT,
     FOREIGN KEY (player_id) REFERENCES Players(number)
 )";
@@ -49,5 +62,27 @@ if ($conn->query($sql) === TRUE) {
 } else {
     echo "Error creating table: " . $conn->error;
 }
+
+// Seediamos las preguntas
+$questionSeed = array('quien?', 'como?', 'cuando?', 'donde?'); // Placeholders
+
+// Revision de la tabla
+$result = $conn->query("SELECT COUNT(*) as total FROM Questions");
+$row = $result->fetch_assoc();
+
+if ($row['total'] == 0) {
+    // Insercion de preguntas
+    foreach ($questionSeed as $question) {
+        $sql = "INSERT INTO Questions (questions) VALUES ('$question')";
+        if ($conn->query($sql) === TRUE) {
+            echo "Pregunta '$question' insertada correctamente.<br>";
+        } else {
+            echo "Error insertando '$question': " . $conn->error . "<br>";
+        }
+    }
+} else {
+    echo "la tabla tiene algo";
+}
+
 
 $conn->close();
--- test/test.php
+++ test/test.php
@@ -4,3 +4,6 @@
 Ete archivo va a tener hardcodeado un ejemplo de trabajo.
 Va a crear los jugadores, meterlos en el juego, hacer un par de rondas.
 */
+
+$preguntas = array('quien?', 'como?', 'cuando?', 'donde?');
+
