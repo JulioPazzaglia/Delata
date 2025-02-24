79f6a5ed3e49934d8653108f0b37addda1a14484
Julio Pazzaglia <julitopazzaglia@gmail.com>
2025-02-24 00:11:45 -0300

    Game creation
    Cree la funcion de Game ceration. Ahora se crea el jeugo y se le agregan 3 preguntas al azar. Deberia volver varible la cantidad de preguntas.

--- gameCreation.php
+++ gameCreation.php
@@ -1,14 +1,35 @@
 <?php
 include("DBconfig.php");
 
+
 /*
 Creacion de la tabla game
 */
 
-function createGame() {
-    $sql = "INSERT INTO Game (questions) VALUES ('')";
-    
+function fetchQuestions($conn)
+{
+    $sql = "SELECT questions FROM Questions";
+    $result = $conn->query($sql);
+    $questionsArray = [];
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $questionsArray[] = $row['questions'];
+        }
+    }
+    return $questionsArray;
 }
+function createGame($conn)
+{
+    $questions = fetchQuestions($conn);
+    shuffle($questions);
+    $questions = array_slice($questions, 0, 3);
+    $questions = implode(", ", $questions);
 
-
-$conn->close();
+    $sql = "INSERT INTO `game`(`questions`) VALUES ('[$questions]');";
+    if ($conn->query($sql) === TRUE) {
+        echo "Game created succesfully";
+    } else {
+        echo "Error creating game: " . $conn->error;
+    }
+}
--- test/test.php
+++ test/test.php
@@ -1,9 +1,9 @@
 <?php
+include("../gameCreation.php");
 
+createGame($conn);
 /*
 Ete archivo va a tener hardcodeado un ejemplo de trabajo.
 Va a crear los jugadores, meterlos en el juego, hacer un par de rondas.
 */
-
-//
-
+$conn->close();
